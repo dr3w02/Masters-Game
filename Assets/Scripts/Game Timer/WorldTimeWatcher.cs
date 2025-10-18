37d82b43509901264574050dc37dd7fdc9d0ba6d
dr3w02 <146147286+dr3w02@users.noreply.github.com>
2025-10-18 22:19:21 +1300

    day_night_cycle
    Add Lighting day to night cycle on timer
    
    Added an event trigger system at certain times

--- Assets/Scripts/Game Timer/WorldTimeWatcher.cs
+++ Assets/Scripts/Game Timer/WorldTimeWatcher.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using System;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using System.Linq;
 
-public class WorldTimeWatcher : MonoBehaviour
+namespace WorldTime
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    public class WorldTimeWatcher : MonoBehaviour
     {
-        
+        [SerializeField]
+        private WorldTime _worldTime;
+
+
+        //List to store all the scheduled events
+        [SerializeField]
+        private List<Schedule> _schedule;
+
+        private void Start()
+        {
+            _worldTime.WorldTimeChanged += CheckSchedule;
+        }
+
+        private void OnDestory()
+        {
+            _worldTime.WorldTimeChanged -= CheckSchedule;
+        }
+
+        //Losts inside of the schedual list and checks if any of the listed events is set for the time of the day that is now
+        //if there is then invoke the action listed in the unity event 
+        private void CheckSchedule(object sender, TimeSpan newTime)
+        {
+            var schedule = _schedule.FirstOrDefault( s => s.Hour == newTime.Hours && s.Minute == newTime.Minutes);
+
+            schedule?._action?.Invoke();
+        }
+
+
+        //Setting up a event system where when something happens and this hour or minute the event begins
+        [Serializable]
+        private class Schedule 
+        {
+            public int Hour;
+            public int Minute;
+
+            public UnityEvent _action;
+        }
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
